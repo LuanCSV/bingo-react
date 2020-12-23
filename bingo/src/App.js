ba13f421c6d5bf37e270444fb9704865b209c2ff
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-12-23 11:14:23 -0300

    React adicionado

---
+++ bingo/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bingo virtual
+        </p>
+      </header>
+      <main className="App-body">
+        <div>
+          <p>alo</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
